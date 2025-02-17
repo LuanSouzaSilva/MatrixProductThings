dca0f704f0757230e2d65d5f1de4434df6aea89b
LuanSS <luansouzasilva@yahoo.com.br>
2025-02-17 16:21:09 -0300

    Adicao do max bond dimension

--- MPS_App.py
+++ MPS_App.py
@@ -3,8 +3,9 @@ import numpy as np
 #importa as funções do arquivo MPS_Func
 from MPS_Func import *
 
-Nsites = 6 #Numero de sistios
+Nsites = 12 #Numero de sitios
 Nbase = Nsites #Numero de estados "base" para a construcao do estado desejado
+max_bond_dimension = 4
 dim = []
 for i in range(Nsites):
     dim.append(2)#Dimensao do espaco de Hilbert local
@@ -21,7 +22,7 @@ dim = tuple(dim)
 js = np.zeros((Nsites, 2))
 js[:, 1] = 1
 
-Us, SingVals = CalculatePreMatrices(Nsites, js, states, coeffs, dim) #Calculo dos Us e dos valores singulares
+Us, SingVals = CalculatePreMatrices(Nsites, js, states, coeffs, dim, max_bond_dimension) #Calculo dos Us e dos valores singulares
 
 As = ContractSVals(Us, SingVals) #Contracao dos Us com os valores singulares
 
@@ -31,20 +32,20 @@ for i in range(len(As)):
     print('Matriz indice 0: \n', np.round(As[i][0], 3), '\n')
     print('Matriz indice 1: \n', np.round(As[i][1], 3), '\n\n')
 
-labels_ = []
-newindex_ = []
-count = 0
-for label in product(range(0, 2), repeat=Nsites): 
-    labels_.append(list(label))
-    newindex_.append(count)
+# labels_ = []
+# newindex_ = []
+# count = 0
+# for label in product(range(0, 2), repeat=Nsites): 
+#     labels_.append(list(label))
+#     newindex_.append(count)
 
-    count += 1
+#     count += 1
 
-rho = DensityMatrix(np.array(newindex_), np.array(labels_), As, Nsites)
+# rho = DensityMatrix(np.array(newindex_), np.array(labels_), As, Nsites)
 
-print(np.max(np.linalg.eigvalsh(rho)))
+# print(np.max(np.linalg.eigvalsh(rho)))
 
-import matplotlib.pyplot as plt
-plt.imshow(rho[:100, :100])
-plt.colorbar()
-plt.show()
+# import matplotlib.pyplot as plt
+# plt.imshow(rho[:100, :100])
+# plt.colorbar()
+# plt.show()
--- MPS_Func.py
+++ MPS_Func.py
@@ -3,13 +3,16 @@ from scipy.linalg import svd
 from itertools import product
 from numba import njit, prange
 
+#Cria o estado inicial como um tensor da forma
+#|psi> = sum_{j_1...j_Ns} C_{j_1 ... j_Ns} |j_1...j_Ns>
 def InitialState(states_, coeffs_, dim_):
-    C_ = np.zeros(dim_) #tensor C_{j1 j2 j3 j4}
+    C_ = np.zeros(dim_) #tensor C_{j_1 j_2 ... j_Ns}
     for i in range(len(states_)):
-        C_[tuple(states_[i, :])] = coeffs_[i]
-    return C_/np.sqrt(np.vdot(coeffs_, coeffs_))
+        C_[tuple(states_[i, :])] = coeffs_[i] #Coeficientes da base no tensor
+    return C_/np.sqrt(np.vdot(coeffs_, coeffs_)) #Normalizacao
                  
-
+#Agrupa indices. Por exemplo, considere os indices j_1, j_2, j_3 e j_4. suponha que queiramos juntar j_2, j_3 e j_4 num unico indice
+#esta funcao entao cria o indice k = (j_2 j_3 j_4), que vai de 1 a max(j_2)*max(j_3)*max(j_4)
 def GroupIndex(indexes_):
 
     labels_ = []
@@ -23,6 +26,7 @@ def GroupIndex(indexes_):
 
     return np.array(labels_), np.array(newindex_)
 
+#Esta funcao agrupa os indices à esquerda com as respectivas bond dimension
 def GroupLeftIndex(index_, bonddim_):
 
     labels_ = []
@@ -37,7 +41,7 @@ def GroupLeftIndex(index_, bonddim_):
 
     return np.array(labels_), np.array(newindex_)
 
-
+#Transforma o tensor C_{j_1, j_2 ... j_Ns} numa matriz inicial
 def Tensor2Matrix(Nsites_, indexes_, states_, coeffs_, dim_):
     C_ = InitialState(states_, coeffs_, dim_)
     labels_, newindex_ = GroupIndex(indexes_[-(Nsites_-1):, :])
@@ -49,60 +53,72 @@ def Tensor2Matrix(Nsites_, indexes_, states_, coeffs_, dim_):
             M_[i, ii] = C_[(i,) + tuple(labels_[ii])]
     return M_
 
+#Aplica decomposicao em valor singular (SVD)
 def ApplySVD(matrix_):
-    return svd(matrix_, full_matrices=False) #Decomposicao em valor singular da primeira matriz
+    return svd(matrix_, full_matrices=False)
 
-def SweepSite(Nsites_, site_, bonddim_, indexes_, matrix_):
-    Right_indexes_ = indexes_[-(Nsites_-site_-1):, :]
+#Faz um "reshape" da matriz correspondente ao sitio informado
+def SweepSite(Nsites_, site_, bonddim_, indexes_, matrix_, maxbd_):
 
-    Rlabels_, k1_ = GroupIndex(Right_indexes_)
-    Llabels_, k2_ = GroupLeftIndex(indexes_[(Nsites_-site_-1), :], bonddim_)
+        Right_indexes_ = indexes_[-(Nsites_-site_-1):, :]
 
-    M_ = np.zeros((len(k2_), len(k1_)))
+        Rlabels_, k1_ = GroupIndex(Right_indexes_)
+        if bonddim_ < maxbd_:
+            Llabels_, k2_ = GroupLeftIndex(indexes_[(Nsites_-site_-1), :], bonddim_)
+        else:
+            Llabels_, k2_ = GroupLeftIndex(indexes_[(Nsites_-site_-1), :], maxbd_)
+        M_ = np.zeros((len(k2_), len(k1_)))
 
-    for i in range(len(k2_)):
-        for ii in range(len(k1_)):
+        for i in range(len(k2_)):
+            for ii in range(len(k1_)):
 
-            #print(Llabels_[i, 1], len(k1_)*Llabels_[i, 0]+k1_[ii])
-            M_[i, ii] = matrix_[Llabels_[i, 1], len(k1_)*Llabels_[i, 0]+k1_[ii]]
+                M_[i, ii] = matrix_[Llabels_[i, 1], len(k1_)*Llabels_[i, 0]+k1_[ii]]
 
-    return M_
+        return M_
 
-def CalculatePreMatrices(Nsites_, indexes_, states_, coeffs_, dim_):
+#Opera na matriz inicial, transformando-a, da esquerda para a direita, num produto de matrizes 
+def CalculatePreMatrices(Nsites_, indexes_, states_, coeffs_, dim_, maxbd_):
     M_ = Tensor2Matrix(Nsites_, indexes_, states_, coeffs_, dim_)
     U_, Sigma_, Vdag_ = ApplySVD(M_)
 
     Us_ = []
     SingVals_ = []
+    as_ = []
     for i in range(1, Nsites_-1):
         U_, Sigma_, Vdag_ = ApplySVD(M_)
 
         a_ = len(Sigma_)
-        M_ = SweepSite(Nsites_, i, a_, indexes_, Vdag_)
+        M_ = SweepSite(Nsites_, i, a_, indexes_, Vdag_, maxbd_)
 
         Us_.append(U_)
         SingVals_.append(Sigma_)
+        as_.append(a_)
 
     U_, Sigma_, Vdag_ = ApplySVD(M_)
 
     Us_.append(U_)
     Us_.append(Vdag_)
     SingVals_.append(Sigma_)
+    print('Max bond dim: ', max(as_))
 
     return Us_, SingVals_
 
-def ContractSVals(Us_, SingVals_):
-    As_ = []
-    for i in range(len(Us_)-1):
-        As_.append(np.array(np.split(np.matmul(Us_[i], np.diag(SingVals_[i])), 2)))
-    As_.append(np.array(np.split(Us_[-1], 2, axis = 1)))
-
-    return As_
-
+#Multiplicacao de matriz
 @njit(fastmath=True)
 def nb_op(mat, q):
   return mat @ q
 
+#Contrai as matrizes com seus respectivos valores singulares. Isso também eh feito da esquerda para a direita
+def ContractSVals(Us_, SingVals_):
+    As_ = []
+    for i in range(len(Us_)-1):
+        As_.append(np.array(np.split(nb_op(Us_[i], np.diag(SingVals_[i])), 2)))
+    As_.append(np.array(np.split(Us_[-1], 2, axis = 1)))
+
+    return As_
+
+
+#Constroi a matriz de densidade
 @njit(parallel = True)
 def DensityMatrix(newindex_, labels_, As_, Nsites):
     rho_ = np.zeros((len(newindex_), len(newindex_)))
@@ -118,3 +134,35 @@ def DensityMatrix(newindex_, labels_, As_, Nsites):
                 auxj = nb_op(auxj, As_[k+1][labels_[j][k+1]])
             rho_[i][j] = auxi[0][0]*np.conjugate(auxj[0][0])
     return rho_
+
+if __name__ == '__main__':
+    Nsites = 6 #Numero de sistios
+    Nbase = Nsites #Numero de estados "base" para a construcao do estado desejado
+
+    maxbd = 2
+
+    dim = []
+    for i in range(Nsites):
+        dim.append(2)#Dimensao do espaco de Hilbert local
+
+    states = np.zeros((Nbase, Nsites), dtype = int) #Inicializacao dos estados
+    coeffs = np.ones(Nbase) #Inicializacao dos coeficientes dos estados "base"
+    for i in range(Nbase):
+        states[i, i] = 1 #Aqui consideramos que o i-ésimo estado tem spin up no i-ésimo sitio, e todos os outros sao spin down
+        coeffs[i] = 1 #Aqui consideramos que todos os coeficientes sao iguais
+
+    dim = tuple(dim)
+
+    #Valores para os indices
+    js = np.zeros((Nsites, 2))
+    js[:, 1] = 1
+
+    Us, SingVals = CalculatePreMatrices(Nsites, js, states, coeffs, dim, maxbd) #Calculo dos Us e dos valores singulares
+
+    As = ContractSVals(Us, SingVals) #Contracao dos Us com os valores singulares
+
+    #Imprime as matrizes
+    for i in range(len(As)):
+        print(f'Sitio {i+1}:')
+        print('Matriz indice 0: \n', np.round(As[i][0], 3), '\n')
+        print('Matriz indice 1: \n', np.round(As[i][1], 3), '\n\n')
